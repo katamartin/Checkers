9ffd14e90ba53891ef80f85e3bec3e0dc0b9f1fd
katamartin <katasm@gmail.com>
2015-07-02 11:52:59 -0700

    Respond to keypress and perform slide

--- board.rb
+++ board.rb
@@ -1,10 +1,13 @@
 require_relative 'piece'
-
+require 'io/console'
 
 class Board
   attr_reader :grid
+  attr_accessor :cursor, :selected_positions
   def initialize(populate = true)
     @grid = Array.new(8) { Array.new(8) }
+    @cursor = [0, 0]
+    @selected_positions = []
     fill_grid(populate)
   end
 
@@ -33,7 +36,7 @@ class Board
 
   def on_board?(pos)
     return false unless pos.all? { |idx| idx.between?(0, 7) }
-    return false unless (pos[0] + pos[1]).even?
+    # return false unless (pos[0] + pos[1]).even?
 
     true
   end
@@ -48,11 +51,10 @@ class Board
       print "#{i} "
       row.each_with_index do |el, j|
         if el == nil
-          string = "   "
+          string = "   ".colorize(:background => background_color([i, j]))
         else
-          string = " #{el} "
+          string = " #{el} ".colorize(:background => background_color([i, j]))
         end
-        string = string.colorize(:background => :black) if (i + j).odd?
         print string
       end
       puts ""
@@ -61,4 +63,93 @@ class Board
     nil
   end
 
+  def background_color(pos)
+    if pos == cursor
+      return :yellow
+    elsif selected_positions.include?(pos)
+      return :green
+    elsif (pos[0] + pos[1]).odd?
+      return :black
+    else
+      :light_white
+    end
+  end
+
+  def valid_pos?(pos)
+    on_board?(pos) && (pos[0] + pos[1]).even?
+  end
+
+  def empty?(pos)
+    raise "Not a valid piece position" unless valid_pos?(pos)
+
+    self[pos].nil?
+  end
+
+  def read_char
+    STDIN.echo = false
+    STDIN.raw!
+
+    input = STDIN.getc.chr
+    if input == "\e" then
+      input << STDIN.read_nonblock(3) rescue nil
+      input << STDIN.read_nonblock(2) rescue nil
+    end
+
+    ensure
+      STDIN.echo = true
+      STDIN.cooked!
+
+      return input
+  end
+
+  def move_cursor(delta)
+    moved_cursor = add_arrs(cursor, delta)
+    self.cursor = moved_cursor if on_board?(moved_cursor)
+  end
+
+  def select_position(pos)
+    selected_positions << pos
+    if selected_positions.length == 2
+      piece = self[selected_positions.first]
+      piece.perform_slide(selected_positions.last) unless piece.nil?
+      self.selected_positions = []
+      render
+    elsif selected_positions.length > 2
+      self.selected_positions = [pos]
+      render
+    end
+  end
+
+  def use_cursor
+    c = read_char
+
+    case c
+
+    when "\r"
+      select_position(cursor)
+    when "\u0003"
+      raise Interrupt
+    when "\e[A"
+      move_cursor([-1, 0])
+    when "\e[B"
+      move_cursor([1, 0])
+    when "\e[C"
+      move_cursor([0, 1])
+    when "\e[D"
+      move_cursor([0, -1])
+    end
+  end
+
+  def add_arrs(arr1, arr2)
+    raise "Can only add 2D vecs!" unless arr1.length == 2 and arr2.length == 2
+
+    [arr1[0] + arr2[0], arr1[1] + arr2[1]]
+  end
+
+end
+
+b = Board.new
+while true
+  b.render
+  b.use_cursor
 end
--- piece.rb
+++ piece.rb
@@ -1,14 +1,17 @@
 require 'colorize'
 
 class Piece
-  attr_reader :board, :pos, :color
+  attr_accessor :pos
+  attr_reader :board, :color, :kinged, :heading
   def initialize(board, pos, color)
-    raise "Invalid position" unless board.on_board?(pos)
+    raise "Invalid position" unless board.valid_pos?(pos)
     raise "Invalid color" unless [:red, :black].include?(color)
 
     @board = board
     @pos = pos
     @color = color
+    @heading = color == :red ? 1 : -1
+    @kinged = false
     board.add_piece(self, pos)
   end
 
@@ -16,8 +19,41 @@ class Piece
     "●".colorize(color)
   end
 
+  def king?
+    kinged
+  end
+
   def to_s
     inspect
+  end
+
+  def perform_slide(end_pos)
+    return false unless moves.include?(end_pos) && board.empty?(end_pos)
+    update_pos(end_pos)
+
+    true
+  end
+
+  def update_pos(end_pos)
+    board[pos] = nil
+    self.pos = end_pos
+    board[end_pos] = self
+  end
+
+  def move_diffs
+    [[heading, -1], [heading, 1]]
+  end
+
+  def moves
+    possible_moves = move_diffs.map { |delta| add_arrs(delta, pos) }
+
+    possible_moves.select { |move| board.valid_pos?(move) }
+  end
+
+  def add_arrs(arr1, arr2)
+    raise "Can only add 2D vecs!" unless arr1.length == 2 and arr2.length == 2
+
+    [arr1[0] + arr2[0], arr1[1] + arr2[1]]
   end
 
 end
